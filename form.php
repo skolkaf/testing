2f9cc3aee240249c1af7f38e4bcf4b1888212006
František Skolka <f.skolka@seznam.cz>
2020-07-17 10:59:01 +0200

    Ukol2 - 1

---
+++ form.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="cs">
+<body>
+
+<h2>Platba přes platební bránu</h2>
+
+<form action="/action.php" method="POST">
+    <label for="castka">Částka:</label><br>
+    <input type="text" id="castka" name="castka"><br>
+    <input type="submit" value="Zaplatit">
+</form>
+
+
+</body>
+</html>
+
+
+
+
+
---
+++ totojetest.php
@@ -0,0 +1,6 @@
+<?php
+
+
+echo "Toto je test.";
+
+?>
